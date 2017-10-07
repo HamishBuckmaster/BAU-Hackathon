2f4adff1985977cf746ba8f517481a8b35f28fc3
Hamish Buckmaster <h.buckmaster@hotmail.com>
2017-10-07 17:03:50 +1100

    Fixed: Moved to straight PHP
    Master Data, more to come

---
+++ backend/includes/config.php
@@ -0,0 +1,13 @@
+<?php
+//config file for common things.
+
+$DB_CONNECTION="mysql";
+
+$DB_HOST="bauhackathon-mysql.ch51hucil0ep.ap-southeast-2.rds.amazonaws.com";
+$DB_PORT="3306";
+$DB_DATABASE="db";
+$DB_USERNAME="master";
+$DB_PASSWORD="bauhackPass";
+
+
+?>
