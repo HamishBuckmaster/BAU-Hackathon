291d1d14b422421b7a2a738b9043e956fbb195e0
Hamish Buckmaster <h.buckmaster@hotmail.com>
2017-10-07 17:09:24 +1100

    Added: The rest of the DB to php json output

---
+++ backend/promos.php
@@ -0,0 +1,27 @@
+<?php
+require_once('includes/config.php');
+
+// Tell the browser to expect JSON
+header('Content-type: application/json');
+
+// store configuration data elsewhere
+$conn = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
+
+// Create an empty array to contain results
+$json_response = array();
+
+// Select all the items in the database
+$results = mysqli_query($conn, 'SELECT * FROM `3_Promotions`');
+
+while($row = mysqli_fetch_assoc($results)) {
+    // Add the raw contents of each row to the response
+    // You can modify the $row here as well, or filter out certain rows
+    $json_response[] = $row;
+}
+
+
+// Finally, send a JSON representation of the $json_response array
+// to the browser as the request body
+echo json_encode($json_response);
+
+?>
